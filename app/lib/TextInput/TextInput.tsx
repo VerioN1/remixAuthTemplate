14cb6e2b6c35e5a3cfd2d754c2cd1eafc765521c
alonb <alonbarel221@gmail.com>
2022-10-11 11:28:48 +0300

    added input style

--- app/lib/TextInput/TextInput.tsx
+++ app/lib/TextInput/TextInput.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import {useFocusWithin} from '@mantine/hooks';
+import React, {useRef, useState} from 'react';
 import type { TextInputProps } from '@mantine/core';
 import { createStyles, TextInput as MTextInput } from '@mantine/core';
 
@@ -6,19 +7,33 @@ type Props = TextInputProps
 
 
 const useStyles = createStyles((theme) => ({
+	root: {
+		position: 'relative',
+	},
+	label:{
+		position: 'absolute',
+		left: 0,
+		padding: '0.7rem 0 0.5rem',
+		pointerEvents: 'none',
+		letterSpacing: '0.1rem',
+		transition: '0.2s',
+	},
 	input: {
 		borderBottom: '2px solid',
+		background: 'transparent',
 		'&:focus': {
 			borderColor: theme.primaryColor
-		}
+		},
 	}
 }));
 
 const TextInput = ({children ,...props} : Props) => {
 	const {classes} = useStyles()
-
+	const { ref, focused } = useFocusWithin();
+	console.log(focused);
+	const [isFocus, setIsFocus] = useState(false);
 	return (
-		<MTextInput {...props} radius="xl" variant="unstyled" classNames={classes} >
+		<MTextInput ref={ref} onFocus={() => setIsFocus(true)} {...props} radius="xl" labelProps={{className: `${focused ? 'on-input-focus' : ''}`}} variant="unstyled" classNames={classes} >
 			{children}
 		</MTextInput>
 	);
